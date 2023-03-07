2c3972c992ae9bda9e92739a8c3b4b00bc1d9c6e
Utkin <xxwild.duckxx@yandex.ru>
2023-03-08 00:49:54 +0300

    init project

---
+++ src/test/java/ru/timutkin/rabbitmqhelloworld/RabbitMqHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.timutkin.rabbitmqhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
